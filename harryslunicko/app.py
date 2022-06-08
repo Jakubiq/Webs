691d5663a3a0dc3ea93916882a3997ea6929c77e
jakubiq <jakub.martinec99@gmail.com>
2022-06-08 18:27:16 +0200

    Add of text wiht not found city

--- harryslunicko/app.py
+++ harryslunicko/app.py
@@ -7,6 +7,7 @@ app.secret_key = "fortnitebattlepass"
 
 @app.route('/', methods=["GET", "POST"])
 def Weather():
+    found = True
     if request.method == 'POST':
         city = request.form['city']
     else:
@@ -14,15 +15,16 @@ def Weather():
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=b77748418d25e708cc4f30931fc06401&units=metric&lang=cz'
     reply = requests.get(url).json()
     if int(reply['cod']) > 300:
-        city = "Praha"
-        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=b77748418d25e708cc4f30931fc06401&units=metric&lang=cz'
+        city = "Toto město bohužel neexituje"
+        url = f'https://api.openweathermap.org/data/2.5/weather?q=Praha&appid=b77748418d25e708cc4f30931fc06401&units=metric&lang=cz'
+        found = False
         reply = requests.get(url).json()
     temp = reply['main']['temp']
     describe = reply['weather'][0]['description']
     icon = reply['weather'][0]['icon']
     lon = reply['coord']['lon']
-    lat = reply['coord']['lat']
-    return render_template('weather.html', reply=reply, temp=temp, describe=describe, icon=icon, city=city, lon=lon, lat=lat)
+    lat = reply['coord']['lat'] 
+    return render_template('weather.html', reply=reply, temp=temp, describe=describe, icon=icon, city=city, lon=lon, lat=lat, found=found)
 
 
 if __name__ == '__main__':
